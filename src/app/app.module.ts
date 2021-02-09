ab45d8f5e37d28cfa97e04c01647a2f48d68a7e0
Bastian_Blaeser <bastian.blaeser@nterra.com>
2021-02-09 10:17:57 +0100

    added file structure and tree basic components to order some pizza.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PizzaMenuComponent } from './component/pizza-menu/pizza-menu.component';
+import { OrdersComponent } from './component/orders/orders.component';
+import { ShoppingCartComponent } from './component/shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PizzaMenuComponent,
+    OrdersComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/component/pizza-menu/pizza-menu.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PizzaMenuComponent } from './pizza-menu.component';
+
+describe('PizzaMenuComponent', () => {
+  let component: PizzaMenuComponent;
+  let fixture: ComponentFixture<PizzaMenuComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PizzaMenuComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PizzaMenuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
